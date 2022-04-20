6cfee54f34135eacdef44d04a1082757a1c5b67b
SushmaReddy-prog <67220170+SushmaReddy-prog@users.noreply.github.com>
2022-04-20 15:53:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
-function preload()
-{
-	song = loadSound("music.mp3");
+function preload(){
+	song = loadSound("music1.mp3");
 }
 
 function setup() {
@@ -11,13 +14,36 @@ function setup() {
 
 	video = createCapture(VIDEO);
 	video.hide();
+	
+	posenet = ml5.posenet(video , modelLoaded);
+	poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded(){
+	console.log('PoseNet is Initialized');
 }
 
 function draw() {
 	image(video, 0, 0, 600, 500);
 }
 
-function play1()
-{
+function play(){
 	song.play();
+	song.setVolume(1);
+	song.rate(1);
 }
+
+function gotPoses(results){
+	if(results.length > 0){
+		console.log(results);
+
+		leftWristX = results[0].pose.leftWrist.x;
+		leftWristY = results[0].pose.leftWrist.y;
+		console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+		rightWristX = results[0].pose.rightWrist.x;
+		rightWristY = results[0].pose.rightWrist.y;
+		console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+	}
+}
+
